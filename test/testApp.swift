ae25d152739a8ab0a55b03888f6268cfd40ebcea
Aria Chen <ariachen@berkeley.edu>
2023-10-28 17:36:14 -0700

    initialization

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Aria Chen on 2023/10/28.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
